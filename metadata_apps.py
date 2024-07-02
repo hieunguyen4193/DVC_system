dea020d4a12bcfb31c9b149a3925a7618a1c1262
hieunguyen4193 <work@hieunguyen.de>
2024-07-03 00:39:58 +0200

    add utils and update test notebook

---
+++ metadata_apps.py
@@ -0,0 +1,96 @@
+from flask import Flask, render_template_string
+import pandas as pd
+import argparse
+
+from minio_utils import * 
+
+def parse_arguments():
+    parser = argparse.ArgumentParser(description='Run Flask app to display MinIO metadata.')
+    parser.add_argument('--bucket_name', type=str, required=True, help='The name of the MinIO bucket.')
+    parser.add_argument('--credentials', type=str, required=True, help='Path to the MinIO credentials file.')
+    
+    args = parser.parse_args()
+    return args.bucket_name, args.credentials
+
+from flask import Flask, render_template_string
+import pandas as pd
+
+def render_interactive_table(df):
+    html_table = df.to_html(classes='table table-striped table-bordered', index=False, border=0)
+    html_template = """
+    <!DOCTYPE html>
+    <html>
+    <head>
+        <title>Metadata Table with Column Filters</title>
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+        <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.10.21/css/dataTables.bootstrap4.min.css">
+        <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/buttons/1.6.2/css/buttons.bootstrap4.min.css">
+        <script src="https://code.jquery.com/jquery-3.5.1.js"></script>
+        <script src="https://cdn.datatables.net/1.10.21/js/jquery.dataTables.min.js"></script>
+        <script src="https://cdn.datatables.net/1.10.21/js/dataTables.bootstrap4.min.js"></script>
+        <script src="https://cdn.datatables.net/buttons/1.6.2/js/dataTables.buttons.min.js"></script>
+        <script src="https://cdn.datatables.net/buttons/1.6.2/js/buttons.bootstrap4.min.js"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/jszip/3.1.3/jszip.min.js"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.53/pdfmake.min.js"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.53/vfs_fonts.js"></script>
+        <script src="https://cdn.datatables.net/buttons/1.6.2/js/buttons.html5.min.js"></script>
+        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+    </head>
+    <body>
+        <table id="metadataTable" class="table table-striped table-bordered" style="width:100%">
+            <thead>
+                <tr>
+                    {% for column in df.columns %}
+                    <th>{{ column }}</th>
+                    {% endfor %}
+                </tr>
+            </thead>
+            <tbody>
+                {% for row in df.itertuples() %}
+                <tr>
+                    {% for cell in row[1:] %}
+                    <td>{{ cell }}</td>
+                    {% endfor %}
+                </tr>
+                {% endfor %}
+            </tbody>
+        </table>
+        <script>
+        $(document).ready(function() {
+            var table = $('#metadataTable').DataTable({
+                dom: 'Bfrtip',
+                buttons: [
+                    { extend: 'copy', className: 'btn btn-primary' },
+                    { extend: 'csv', className: 'btn btn-primary' },
+                    { extend: 'excel', className: 'btn btn-primary' },
+                    { extend: 'pdf', className: 'btn btn-primary' }
+                ],
+                "pagingType": "full_numbers",
+                "lengthChange": false,
+                "searching": true,
+                "ordering": true,
+                "info": true,
+                "autoWidth": false,
+                "responsive": true,
+            });
+        });
+        </script>
+    </body>
+    </html>
+    """
+    return render_template_string(html_template, df=df)
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    # bucket_name = "dev"
+    # minio_credentials = "credentials_python_minio.json"
+    metadata_dict = extract_metadata_of_all_objects(bucket_name = bucket_name, 
+                                                    minio_credentials = minio_credentials)
+    metadatadf = pd.DataFrame.from_dict(metadata_dict, orient = "index").reset_index()
+    return render_interactive_table(metadatadf)
+
+if __name__ == '__main__':
+    bucket_name, minio_credentials = parse_arguments()
+    app.run(debug=True)
---
+++ minio_utils.py
@@ -0,0 +1,136 @@
+#####-------------------------------------------------------------#####
+##### FUNCTION TO CREATE A NEW BUCKET OF DATA IN MINIO 
+#####-------------------------------------------------------------#####
+import minio
+from minio.error import S3Error
+
+import pandas as pd
+import os
+import json
+from minio.commonconfig import ENABLED
+from minio.versioningconfig import VersioningConfig
+
+def create_bucket(bucket_name, minio_credentials, versioning = True):
+    """
+    Create a bucket in MinIO.
+
+    Parameters:
+    - bucket_name (str): The name of the bucket to create.
+    - minio_client (minio.Minio): An instance of the Minio client.
+
+    Returns:
+    - bool: True if the bucket was created successfully, False otherwise.
+    """
+    with open(minio_credentials, 'r') as file:
+        keys = json.load(file)
+        
+    minio_client = minio.Minio(
+        endpoint = "localhost:9000", # must use API port, not the original port
+        access_key = keys["accessKey"],
+        secret_key = keys["secretKey"],
+        secure = False # FIX ME PLEASE!!! WHAT IS THE BEST WAY TO HANDLE THIS SECURITY ISSUE?  
+    )
+    try:
+        # Check if the bucket already exists
+        exists = minio_client.bucket_exists(bucket_name)
+        if exists == False: 
+            # Make a new bucket
+            minio_client.make_bucket(bucket_name)
+            print(f"Bucket '{bucket_name}' created successfully.")
+            if versioning:
+                minio_client.set_bucket_versioning(bucket_name, VersioningConfig(ENABLED))
+
+        else:
+            print(f"Bucket '{bucket_name}' already exists.")
+        return True
+    except S3Error as e:
+        print(f"Error creating bucket: {e}")
+        return False
+
+#####-------------------------------------------------------------#####
+##### Upload file with metadata
+#####-------------------------------------------------------------#####
+def upload_file_with_metadata(bucket_name, 
+                              object_name, 
+                              file_to_upload, 
+                              metadata, 
+                              minio_credentials,
+                              verbose = False):
+    """
+    Upload a file to a MinIO bucket with specified metadata.
+
+    Parameters:
+    - bucket_name (str): The name of the target bucket.
+    - object_name (str): The object name in the bucket.
+    - file_to_upload (str): The local path to the file to be uploaded.
+    - metadata (dict): A dictionary of metadata to attach to the object.
+    - minio_credentials (str): Path to the JSON file with MinIO credentials.
+
+    Returns:
+    - bool: True if the file was uploaded successfully, False otherwise.
+    """
+    with open(minio_credentials, 'r') as file:
+        keys = json.load(file)
+    
+    minio_client = minio.Minio(
+        endpoint="localhost:9000",
+        access_key=keys["accessKey"],
+        secret_key=keys["secretKey"],
+        secure=False 
+    )
+    
+    try:
+        with open(file_to_upload, 'rb') as file_data:
+            file_stat = os.stat(file_to_upload)
+            minio_client.put_object(
+                bucket_name=bucket_name,
+                object_name=object_name,
+                data=file_data,
+                length=file_stat.st_size,
+                metadata=metadata
+            )
+        if verbose:
+            print(f"File '{object_name}' uploaded successfully with metadata.")
+        return True
+    except S3Error as e:
+        print(f"Error uploading file: {e}")
+        return False
+    
+#####-------------------------------------------------------------#####
+##### Extract all metadata from a bucket in minio
+#####-------------------------------------------------------------#####
+def extract_metadata_of_all_objects(bucket_name, minio_credentials, simplified = False):
+    """
+    Extract metadata of all objects in a specified MinIO bucket.
+
+    Parameters:
+    - bucket_name (str): The name of the MinIO bucket.
+    - minio_credentials (str): Path to the JSON file with MinIO credentials.
+
+    Returns:
+    - dict: A dictionary with object names as keys and their metadata as values.
+    """
+    with open(minio_credentials, 'r') as file:
+        keys = json.load(file)
+    
+    minio_client = minio.Minio(
+        endpoint="localhost:9000",
+        access_key=keys["accessKey"],
+        secret_key=keys["secretKey"],
+        secure=False  # Consider using secure=True in production environments
+    )
+    
+    metadata_dict = {}
+    try:
+        objects = minio_client.list_objects(bucket_name, recursive=True, include_version = True)
+        for obj in objects:
+            obj_info = minio_client.stat_object(bucket_name, obj.object_name)
+            metadata_dict[obj.object_name] = obj_info.metadata
+        print("Metadata extracted successfully.")
+        metadatadf = pd.DataFrame.from_dict(metadata_dict, orient = "index").reset_index()
+        if simplified == True:
+            metadatadf = metadatadf[["index", "x-amz-version-id"] + [item for item in metadatadf.columns if "x-amz-meta-" in item]]
+        return metadatadf
+    except S3Error as e:
+        print(f"Error extracting metadata: {e}")
+        return {}
