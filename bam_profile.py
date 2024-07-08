27c6e24a7d191c76a981d012f684a1dc1a247c4f
hieunguyen4193 <hieunguyen@zeus.lan>
2024-07-08 18:02:05 +0200

    update RDSbucket object

---
+++ bam_profile.py
@@ -0,0 +1,34 @@
+bam_profile  = {
+    "properties": {
+        "Labcode": {
+            "type": "text"
+        },
+        "SequencingID": {
+            "type": "text"
+        },
+        "FileName": {
+            "type": "text"
+        },
+        "FileType": {
+            "type": "text"
+        },
+        "Date": {
+            "type": "date"
+        },
+        "pipeline": {
+            "type": "text"
+        },
+        "project": {
+            "type": "text"
+        },
+        "sub_project": {
+            "type": "text"
+        },
+        "ref_genome": {
+            "type": "text"
+        },
+        "bucket": {
+            "type": "text"
+        }
+    }
+}
